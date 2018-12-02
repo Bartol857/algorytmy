0c227678d2376bd79824a4232ce4dab8de59a1e0
Bartek Kokoszko <bartol857@gmail.com>
2018-12-02 10:20:12 +0100

    Liczby pierwsze

--- src/main/java/liczbyPierwsze.java
+++ src/main/java/liczbyPierwsze.java
@@ -3,21 +3,22 @@ import java.util.Scanner;
 public class liczbyPierwsze {
     public static void main(String[] args) {
 
-            Scanner scanner = new Scanner(System.in);
-            System.out.print("Podaj liczbę całkowitą: ");
-            int zmienna = scanner.nextInt();
+        zadanie1();
 
-            if (zmienna % 2 ==0){
+
+    }
+
+    public static void zadanie1() {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Podaj liczbę całkowitą: ");
+        int zmienna = scanner.nextInt();
+
+
+            if (zmienna > 3 && zmienna % 2 == 0 && zmienna % 3 == 0 && zmienna % 5 == 0) {
                 System.out.print("Liczba "+ zmienna + " nie jest liczbą pierwszą.");
-            }else if (zmienna % 3 == 0){
-                System.out.print("Liczba "+ zmienna + " nie jest liczbą pierwszą.");
-            }else if (zmienna % 5 == 0){
-                System.out.print("Liczba "+ zmienna + " nie jest liczbą pierwszą.");
-            }else {
+            } else
                 System.out.print("Liczba "+ zmienna + " jest liczbą pierwszą.");
-            }
-
-
 
     }
 
